6c1faf3a0352280402d9f25d434957808bce154b
Lilac1cake <125736095+Lilac1cake@users.noreply.github.com>
2025-10-25 10:58:24 +0200

    Add files via upload
    лабораторная 4

--- main.cpp
+++ main.cpp
@@ -1,26 +1,106 @@
+/*#include <iostream>
+int sumDig(int n){
+        int sum=0;
+        while (n!=0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    
+    }
+int main(){
+    int count=0;
+    int x[5];
+    x[0]=989;
+    x[1]=999;
+    x[2]=57;
+    x[3]=88;
+    x[4]=48;
+    for(int i=0;i<5;++i){
+        if(sumDig(x[i])>25){
+            count++;
+        }
+
+    }
+    if(count>=2){
+        std::cout<<"There are >=2 numbers with sum of digits>25"<<std::endl;
+        for(int i=0;i<4;++i){
+        for(int j=i+1;j<5;++j){
+            if(x[i]>x[j]){
+                std::swap(x[i],x[j]);
+            }
+        }
+    }
+    for (int i=0;i<5;++i){
+        std::cout<<x[i]<<std::endl;
+    }
+
+    }
+    else{
+        std::cout<<"series of numbers does not match the condition"<<std::endl;
+    }
+
+    
+    return 0;
+}
+*/
+
+
 #include <iostream>
-#include <cmath>
+int main(){
+    const int length=4;//cтолбцы
+    const int width=3;//строки
+    int matrix[width][length]={
+        {-1,5,8,-7},
+        {4,-3,6,9},
+        {6,-9,3,8}, 
+    };
+    //подсчет положительных в столбцах
+    int positiveCount[length]={0};
+    for(int j=0;j<length;++j){
+        for(int i=0;i<width;++i){
+            if (matrix[i][j]>0){
+                positiveCount[j]++;
+            }
 
-#include <limits>
-#include <cfloat>
-//int для целых значений сторон трапеции
-//short для значения высоты,т.к. оно занимает мало места(<=255)
-/*float для дробных значений площади*/
-int main() {
-      int x;
-      int y;
-      short z;
-      std::cin>>x>>y>>z;
-      float w= ((x+y)/2)*z ;
-     
-      std::cout <<"answer="<<w<<" "<<"float"<<"\n";
-      std::cout<<"float: min="<<std::numeric_limits<float>::min()<<" "<<"max="<<std::numeric_limits<float>::max()<<" "<<"size="<<"32"<<"\n";
-      std::cout<<"int: min="<<std::numeric_limits<int>::min()<<" "<<"max="<<std::numeric_limits<int>::max()<<" "<<"size="<<"16"<<"\n";
-      std::cout<<"short: min="<<std::numeric_limits<short>::min()<<" "<<"max="<<std::numeric_limits<short>::max()<<" "<<"size="<<"16";
+        }
+    
+    }
+    //проверка, что есть хотя бы одно положительное
+    bool hasPositive=false;
+    for(int j=0;j<length;++j){
+        if(positiveCount[j]>0){
+            hasPositive=true;
+            break;
+        }
+    }
+    //столбец с мин количеством положительных
+    int minCol=0;
+    if(hasPositive){
+        for(int j=1;j<length;++j){
+        if(positiveCount[j]<positiveCount[minCol]){
+            minCol=j;
+        
+        }
+        
+    }
+    for(int i=0;i<width;++i){
+        matrix[i][minCol]=100;
+    }
 
-
-      
-
-
-      return 0;
+    }
+    else{
+        
+        std::cout<<"no positive"<<std::endl;
+    }
+    
+    
+    std::cout<<"Result matrix: "<<std::endl;
+    for (int i=0;i<width;++i){
+        for(int j=0;j<length;++j){
+            std::cout<<matrix[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+    return 0;
 }
